ffdae29187f7cc96cb590a21d1cb425e5151c72c
robibhattacharjee <robibhatt@gmail.com>
2018-11-22 20:07:27 -0800

    cleaned dice.py

--- dice.py
+++ dice.py
@@ -8,24 +8,12 @@ class Dice_roller:
         self.window_size = window_size
         self.prev_rolls = [None for i in range(window_size)]
 
-    def construct_thresholds(self):
-        
-
-
     def query(self):
         p = [self.dice_dict[key] for key in self.keys]
         print p
-        return choice(self.keys, 100, p=p)
+        return choice(self.keys, 1, p=p)[0]
 
-my_dice = {}
-p = 0.6
-my_dice[0] = 1-p
-my_dice[1] = p
 
-my_roller = Dice_roller(my_dice, 10)
-bob =  my_roller.query()
-print len(bob)
-print sum(bob)
 
         
 
